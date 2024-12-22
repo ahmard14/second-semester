cebead31222ff62d6869ca87792bc9b563f43485
ahmard14 <114979581+ahmard14@users.noreply.github.com>
2024-12-22 08:31:50 +0100

    Add files via upload

---
+++ ahmadzayyanuassignment.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+C:\Users\USER\Documents\ahmadzayyanuassignment.sh
+chmod +x ahmadzayyanuassignment.sh
+./ahmadzayyanuassignment.sh
+#!/bin/bash
+
+# Print name and today's date
+echo "Welcome! This is Ahmad Zayyanu's Bash script."
+
+
+
+echo -n "Ahmad Zayyanu: "
+read ID
+echo "Hello, $ALT/SOE/024/0102! Nice to meet you."
+
+# Print numbers from 1 to 10 using a loop
+echo "Here are numbers from 1 to 10:"
+for i in {1..10}
+do
+  echo -n "$i "
+done
+
